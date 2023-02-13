3c1c0201f67191d3601ec0af76fd61d2449630d1
Angus <70203823+ajhawkings@users.noreply.github.com>
2023-02-13 12:00:52 +0000

    Fix index and _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 import fs from 'fs'
 
 import Head from 'next/head'
@@ -21,51 +22,47 @@ interface Props {
 export default function Landing({ files }: Props) {
   const [current, setCurrent] = useState(0)
 
-  return (
-    <>
-      <Head>
-        <title>Conrad Margoles Architects</title>
-      </Head>
-      {current !== 0 &&
-        <button className={styles.arrow} onClick={() => setCurrent(current - 1)}>
-          <Image
-            src="/images/handwritten/Arrows Left Black.png"
-            alt="Previous image"
-            width="50"
-            height="50"
-          />
-        </button>
-      }
-      {current !== files.length - 1 &&
-        <button className={`${styles.arrow} ${styles.right}`} onClick={() => setCurrent(current + 1)}>
-          <Image
-            src="/images/handwritten/Arrows Right Black.png"
-            alt="Next image"
-            width="50"
-            height="50"
-          />
-        </button>
-      }
-      <Link href="/home">
+  return <>
+    <Head>
+      <title>Conrad Margoles Architects</title>
+    </Head>
+    {current !== 0 &&
+      <button className={styles.arrow} onClick={() => setCurrent(current - 1)}>
         <Image
-          src="/images/handwritten/CAArchitects_LogoWhite.png"
-          alt="Conrad Margoles Architects logo"
-          className={styles.logo}
-          width="300"
-          height="100"
+          src="/images/handwritten/Arrows Left Black.png"
+          alt="Previous image"
+          width="50"
+          height="50"
         />
-        <section className={styles.slider}>
-          {files.map((file, i) => {
-            return (
-              <div className={i === current ? `${styles.slide} ${styles.active}` : styles.slide} key={i}>
-                {i === current && (
-                  <Image src={`/images/home/${file}`} alt="Architectural project" className={styles.image} width="2560" height="1709" />
-                )}
-              </div>
-            )
-          })}
-        </section>
-      </Link>
-    </>
-  )
+      </button>
+    }
+    {current !== files.length - 1 &&
+      <button className={`${styles.arrow} ${styles.right}`} onClick={() => setCurrent(current + 1)}>
+        <Image
+          src="/images/handwritten/Arrows Right Black.png"
+          alt="Next image"
+          width="50"
+          height="50"
+        />
+      </button>
+    }
+    <Link href="/portfolio">
+      <Image
+        src="/images/handwritten/CAArchitects_LogoWhite.png"
+        alt="Conrad Margoles Architects logo"
+        className={styles.logo}
+        width="300"
+        height="100"
+      />
+      <section className={styles.slider}>
+        {files.map((file, i) => (
+          <div className={i === current ? `${styles.slide} ${styles.active}` : styles.slide} key={i}>
+            {i === current && (
+              <Image src={`/images/home/${file}`} alt="Architectural project" className={styles.image} width="2560" height="1709" />
+            )}
+          </div>
+        ))}
+      </section>
+    </Link>
+  </>
 }
