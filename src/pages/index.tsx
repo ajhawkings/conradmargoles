37ce292c3692f7e0b54d81b9eda9dac93b78310f
Angus <70203823+ajhawkings@users.noreply.github.com>
2023-03-04 10:27:46 +0000

    Fix button sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Landing ({ files }: Props) {
         <Image
           src="images/handwritten/Arrows Left Black.png"
           alt="Previous image"
-          width="100"
-          height="100"
+          width="50"
+          height="50"
         />
       </button>
     }
@@ -40,8 +40,8 @@ export default function Landing ({ files }: Props) {
         <Image
           src="images/handwritten/Arrows Right Black.png"
           alt="Next image"
-          width="100"
-          height="100"
+          width="50"
+          height="50"
         />
       </button>
     }
