00db27f63999aedc94986ddbee7d25a0820d98cc
Angus <70203823+ajhawkings@users.noreply.github.com>
2023-07-06 20:40:25 +0100

    Preserve scroll

---
+++ src/lib/usePreserveScroll.ts
@@ -0,0 +1,33 @@
+
+import { useEffect, useRef } from 'react'
+import { useRouter } from 'next/router'
+
+export const usePreserveScroll = () => {
+  const router = useRouter()
+
+  const scrollPositions = useRef<{ [url: string]: number }>({})
+
+  useEffect(() => {
+    const onRouteChangeStart = () => {
+      const url = router.pathname
+      scrollPositions.current[url] = window.scrollY
+    }
+
+    const onRouteChangeComplete = (url: string) => {
+      if (scrollPositions.current[url]) {
+        window.scroll({
+          top: scrollPositions.current[url],
+          behavior: 'auto',
+        })
+      }
+    }
+
+    router.events.on('routeChangeStart', onRouteChangeStart)
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    return () => {
+      router.events.off('routeChangeStart', onRouteChangeStart)
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [router])
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { usePreserveScroll } from '@lib/usePreserveScroll'
+
 import '@styles/styles.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App ({ Component, pageProps }: AppProps) {
+  usePreserveScroll()
+
   return <Component {...pageProps} />
 }
