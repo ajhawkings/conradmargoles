b92de6617593ffdc2173cda02cb967006ec153db
Angus <70203823+ajhawkings@users.noreply.github.com>
2023-12-19 23:49:30 +0000

    Update next-image-export-optimizer

--- next.config.js
+++ next.config.js
@@ -12,9 +12,9 @@ module.exports = {
   env: {
     nextImageExportOptimizer_imageFolderPath: "public/images",
     nextImageExportOptimizer_exportFolderPath: "out",
-    nextImageExportOptimizer_quality: 75,
-    nextImageExportOptimizer_storePicturesInWEBP: true,
+    nextImageExportOptimizer_quality: "75",
+    nextImageExportOptimizer_storePicturesInWEBP: "true",
     nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
-    nextImageExportOptimizer_generateAndUseBlurImages: true,
+    nextImageExportOptimizer_generateAndUseBlurImages: "true",
   },
 }
