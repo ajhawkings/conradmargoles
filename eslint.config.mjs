7fafb64eaa8ee492a3f8d73f0eca9cf7653dff67
ajhawkings <70203823+ajhawkings@users.noreply.github.com>
2026-05-19 14:23:47 +0100

    Upgrade to Node 24 and Next 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,35 @@
-/* eslint-disable import/no-anonymous-default-export */
-import { FlatCompat } from "@eslint/eslintrc";
+import { globalIgnores } from 'eslint/config'
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import nextTypescript from 'eslint-config-next/typescript'
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
-
-export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  { ignores: [".next/*"] },
+const config = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
   {
-    files: ["*.ts", "*.tsx"],
+    files: ['**/*.{ts,tsx}'],
     rules: {
-      indent: ["error", 2],
-      quotes: ["error", "single"],
-      semi: ["error", "never"],
-      camelcase: ["error"],
-      eqeqeq: ["error"],
-      "dot-notation": ["error"],
-      "no-return-assign": ["error"],
-      "no-var": ["error"],
-      "prefer-const": ["error"],
-      "sort-imports": ["error", { allowSeparatedGroups: true }],
-      "spaced-comment": ["error"],
-      "space-before-function-paren": ["error", "always"],
-      "object-curly-spacing": ["error", "always"],
-      "eol-last": ["error", "always"],
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      camelcase: ['error'],
+      eqeqeq: ['error'],
+      'dot-notation': ['error'],
+      'no-return-assign': ['error'],
+      'no-var': ['error'],
+      'prefer-const': ['error'],
+      'sort-imports': ['error', { allowSeparatedGroups: true }],
+      'spaced-comment': ['error'],
+      'space-before-function-paren': ['error', 'always'],
+      'object-curly-spacing': ['error', 'always'],
+      'eol-last': ['error', 'always'],
     },
   },
-];
+]
+
+export default config
